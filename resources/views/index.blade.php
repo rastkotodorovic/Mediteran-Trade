046bb344c5c13446eba7e75753236ee85f282b69
rastkotodorovic <rale.todorovic2@gmail.com>
2020-08-12 13:03:47 +0000

    SEO impovements

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -670,41 +670,41 @@
     </div> <!-- /.boxed -->
 
 <!-- Javascript -->
-<script type="text/javascript" src="javascript/jquery.min.js"></script>
-<script type="text/javascript" src="javascript/tether.min.js"></script>
-<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
-<script type="text/javascript" src="javascript/owl.carousel.js"></script>
-<script type="text/javascript" src="javascript/jquery.easing.js"></script>
-<script type="text/javascript" src="javascript/parallax.js"></script>
-<script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
-<script type="text/javascript" src="javascript/waypoints.min.js"></script>
+<script async type="text/javascript" src="javascript/jquery.min.js"></script>
+<script async type="text/javascript" src="javascript/tether.min.js"></script>
+<script async type="text/javascript" src="javascript/bootstrap.min.js"></script>
+<script async type="text/javascript" src="javascript/owl.carousel.js"></script>
+<script async type="text/javascript" src="javascript/jquery.easing.js"></script>
+<script async type="text/javascript" src="javascript/parallax.js"></script>
+<script async type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+<script async type="text/javascript" src="javascript/waypoints.min.js"></script>
 
-<script type="text/javascript" src="javascript/kinetic.js"></script>
-<script type="text/javascript" src="javascript/isotope.pkgd.min.js"></script>
-<script type="text/javascript" src="javascript/imagesloaded.pkgd.min.js"></script>
-<script type="text/javascript" src="javascript/jquery-countTo.js"></script>
-<script type="text/javascript" src="javascript/jquery.owl-filter.js"></script>
-<script type="text/javascript" src="javascript/jquery.fancybox.js"></script>
-<script type="text/javascript" src="javascript/jquery.cookie.js"></script>
-<script type="text/javascript" src="javascript/jquery-validate.js"></script>
-<script type="text/javascript" src="javascript/main.js"></script>
+<script async type="text/javascript" src="javascript/kinetic.js"></script>
+<script async type="text/javascript" src="javascript/isotope.pkgd.min.js"></script>
+<script async type="text/javascript" src="javascript/imagesloaded.pkgd.min.js"></script>
+<script async type="text/javascript" src="javascript/jquery-countTo.js"></script>
+<script async type="text/javascript" src="javascript/jquery.owl-filter.js"></script>
+<script async type="text/javascript" src="javascript/jquery.fancybox.js"></script>
+<script async type="text/javascript" src="javascript/jquery.cookie.js"></script>
+<script async type="text/javascript" src="javascript/jquery-validate.js"></script>
+<script async type="text/javascript" src="javascript/main.js"></script>
 
 
 <!-- Revolution Slider -->
-<script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
-<script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
-<script type="text/javascript" src="revolution/js/slider_v3.js"></script>
+<script async type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
+<script async type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script async type="text/javascript" src="revolution/js/slider_v3.js"></script>
 
 <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script async type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
 
 </body>
 </html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -209,16 +209,16 @@
 	<script src="{{ asset('js/app.js') }}"></script>
 
 	<!-- Javascript -->
-	<script type="text/javascript" src="javascript/jquery.min.js"></script>
-	<script type="text/javascript" src="javascript/tether.min.js"></script>
-	<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
-	<script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
-	<script type="text/javascript" src="javascript/owl.carousel.js"></script>
-	<script type="text/javascript" src="javascript/jquery.easing.js"></script>
-	<script type="text/javascript" src="javascript/parallax.js"></script>
+	<script async type="text/javascript" src="javascript/jquery.min.js"></script>
+	<script async type="text/javascript" src="javascript/tether.min.js"></script>
+	<script async type="text/javascript" src="javascript/bootstrap.min.js"></script>
+	<script async type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+	<script async type="text/javascript" src="javascript/owl.carousel.js"></script>
+	<script async type="text/javascript" src="javascript/jquery.easing.js"></script>
+	<script async type="text/javascript" src="javascript/parallax.js"></script>
 
-	<script type="text/javascript" src="javascript/jquery.cookie.js"></script>
-	<script type="text/javascript" src="javascript/waypoints.min.js"></script>
-	<script type="text/javascript" src="javascript/main.js"></script>
+	<script async type="text/javascript" src="javascript/jquery.cookie.js"></script>
+	<script async type="text/javascript" src="javascript/waypoints.min.js"></script>
+	<script async type="text/javascript" src="javascript/main.js"></script>
 </body>
 </html>
