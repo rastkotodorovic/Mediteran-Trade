cd97ca6ad23a6f3006435a116b60bfdeba38e240
rastkotodorovic <rale.todorovic2@gmail.com>
2020-08-12 13:32:12 +0000

    SEO

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,6 +42,10 @@
     <link rel="preload" href="stylesheet/fancybox.css" as="style">
     <link rel="stylesheet" type="text/css" href="stylesheet/fancybox.css">
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" media="all" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&display=swap" media="all" />
+
     <!-- Theme style -->
     <link rel="preload" href="stylesheet/style.css" as="style">
 	<link rel="stylesheet" type="text/css" href="stylesheet/style.css">
