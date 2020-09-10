bc61cf3ff2e848a557089746905d17f1cf9c0c5e
rastkotodorovic <rale.todorovic2@gmail.com>
2020-09-10 11:43:59 +0000

    dynamic icons

--- database/migrations/2020_09_10_114026_add_icon_to_services_table.php
+++ database/migrations/2020_09_10_114026_add_icon_to_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateServicesTable extends Migration
+class AddIconToServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateServicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('services', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
+        Schema::table('services', function (Blueprint $table) {
+            $table->string('icon')->nullable();
         });
     }
 
@@ -28,6 +25,8 @@ class CreateServicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('services');
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
     }
 }
